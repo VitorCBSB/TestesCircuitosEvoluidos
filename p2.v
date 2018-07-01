2d91e8a2f0f7208f9ade3f7daf5219e48dd3e79f
Vitor Coimbra de Oliveira <vitorcoliveira91@gmail.com>
2018-07-01 10:50:00 -0300

    Revert "Trocada implementação do P2"
    This reverts commit b465cabcff39c231dc34716d6630b461653a7f8b.

--- p2.v
+++ p2.v
@@ -3,7 +3,7 @@ module P2
 	, output out
 	);
 	
-	parameter NUM_GATES = 6;
+	parameter NUM_GATES = 8;
 	
 	wire [NUM_GATES - 1:0] LW;
 	wire [NUM_GATES - 1:0] w;
@@ -18,13 +18,15 @@ module P2
 	end
 	endgenerate
 	
-	nor (LW[0], w[3], w[4]);
-	nor (LW[1], w[4], in[0]);
-	and (LW[2], w[4], in[1]);
-	nor (LW[3], w[2], w[0]);
-	and (LW[4], in[0], w[1]);
-	xnor (LW[5], w[3], in[1]);
+	nand (LW[0], w[3], in[0]);
+	xor (LW[1], w[2], w[5]);
+	xor (LW[2], in[0], w[0]);
+	nor (LW[3], in[0], w[5]);
+	xnor (LW[4], w[4], w[0]);
+	xnor (LW[5], w[7], in[1]);
+	xor (LW[6], in[1], w[4]);
+	xnor (LW[7], w[4], in[0]);
 	
-	assign out = w[5];
+	assign out = w[1];
 	
 endmodule
